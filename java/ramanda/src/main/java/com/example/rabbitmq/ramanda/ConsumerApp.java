c0031786c4ddd36f02d2cacd6537e315a65d56b3
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-13 20:43:18 +0700

    java rabbitmq consumer

---
+++ java/ramanda/src/main/java/com/example/rabbitmq/ramanda/ConsumerApp.java
@@ -0,0 +1,30 @@
+package com.example.rabbitmq.ramanda;
+
+import com.rabbitmq.client.*;
+
+public class ConsumerApp {
+
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri("amqp://guest:guest@localhost:5672/");
+        factory.setVirtualHost("/");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println(message.getEnvelope().getRoutingKey());
+            System.out.println(new String(message.getBody()));
+        };
+
+        CancelCallback cancelCallback = consumerTag -> {
+            System.out.println("Consumer is canceled");
+        };
+
+        channel.basicConsume("whatsapp", true, deliverCallback, cancelCallback);
+//        
+//        channel.close();
+//        connection.close();
+    }
+
+}
---
+++ java/ramanda/src/main/java/com/example/rabbitmq/ramanda/RamandaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq.ramanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamandaApplication.class, args);
+	}
+
+}
